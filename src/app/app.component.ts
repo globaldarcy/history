6eb55556727a980e8a7e97af4905ddaa2a454cc7
Darcy <zhifeng.liu@accenture.com>
2017-12-19 14:34:56 +0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import WatchJS from 'melanke-watchjs';
 
 @Component({
   selector: 'app-root',
@@ -12,9 +13,19 @@ export class AppComponent {
   countChange() {
     console.log(this.allLists);
     // console.log(this.allCount);
-  }
-  constructor() {
-    //this.allCount = this.allLists.length;
+    let ex1 = {
+      attr1: 'initial value of attr1',
+      attr2: 'initial value of attr2'
+    };
+    WatchJS.watch(ex1, 'attr1', function () {
+      console.log('attr1 changed!');
+      console.log(ex1.attr1);
+    });
+    ex1.attr1 = 'other value';
   }
 
+  constructor() {
+    // this.allCount = this.allLists.length;
+  }
 }
+
---
+++ src/app/watchjs/index.d.ts
@@ -0,0 +1,7 @@
+﻿declare module 'melanke-watchjs' {
+  export default class WatchJs {
+    static watch(obj: Object, value: string, watchCallback: Function): void;
+  }
+}
+
+
